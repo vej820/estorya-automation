8f382a0882f61a77cdcc5f1a399c01e4e9a93fba
vej820 <vegie@skunkworks.ai>
2025-01-28 13:17:22 +0800

    Added new test case

--- helpers/school_mod_login.js
+++ helpers/school_mod_login.js
@@ -1,5 +1,5 @@
 async function login(page) {
-    await page.goto('https://estorya-portal-staging-pazl6.ondigitalocean.app/login');
+    await page.goto('https://estorya-portal-staging-pazl6.ondigitalocean.app/login', { timeout: 60000 }); // Increase timeout to 60 seconds
     await page.getByRole('textbox', { name: 'email' }).fill('marygrace+schoolmod@skunkworks.ai');
     await page.getByRole('textbox', { name: 'password' }).fill('@apusLPT2023');
     await page.locator('//*[@id="login"]/div/div/div/div/div/form/button').click();
--- tests/schoolMod.spec.js
+++ tests/schoolMod.spec.js
@@ -4,12 +4,12 @@ import { login } from '../helpers/school_mod_login.js';
 
 
 test('has Login button', async ({ page }) => {
-    await page.goto('https://estorya-portal-staging-pazl6.ondigitalocean.app/login');
+    await page.goto('https://estorya-portal-staging-pazl6.ondigitalocean.app/login', { timeout: 60000 }); // Increase timeout to 60 seconds
     await expect(page.getByText('Login')).toBeVisible();
 });
 
 test('is not able to login with invalid credential', async ({ page }) => {
-    await page.goto('https://estorya-portal-staging-pazl6.ondigitalocean.app/login');
+    await page.goto('https://estorya-portal-staging-pazl6.ondigitalocean.app/login', { timeout: 60000 }); // Increase timeout to 60 seconds
     await page.getByRole('textbox', { name: 'email' }).fill('marygrace+schooldiv@skunkworks.ais');
     await page.getByRole('textbox', { name: 'password' }).fill('@apusLPT2023');
     await page.locator('//*[@id="login"]/div/div/div/div/div/form/button').click();
@@ -19,14 +19,14 @@ test('is not able to login with invalid credential', async ({ page }) => {
 
 test('is able to login with valid credential', async ({ page }) => {
     await login(page);
-    await expect(page.locator('//*[@id="dashboard"]/div[1]/div/div/div/img')).toBeVisible();
+    await expect(page.getByText('Your School’s performance and')).toBeVisible();
 
 });
 
 
 test('add teacher without assigning to classroom', async ({ page }) => {
     await login(page);
-    await expect(page.locator('//*[@id="dashboard"]/div[1]/div/div/div/img')).toBeVisible();
+    await expect(page.getByText('Your School’s performance and')).toBeVisible();
     await page.click('a.btn.btn-estoryalight.w-100');
     await expect(page.getByText('User Management')).toBeVisible();
     await page.click('//*[@id="users"]/div/div/div[1]/div[2]/div/button');
@@ -54,10 +54,21 @@ test('add teacher and assign to classroom', async ({ page }) => {
     await page.fill('#email', 'vegie+auto1@skunkworks.ai');
     await page.selectOption('#role', 'Teacher');
     await page.click('#isAssignClassroom');
-    await page.click('label[for="classroom"]');
-    // await page.waitForSelector('#react-select-6-listbox');
-    console.log(await page.isVisible('.css-14h4o58-menu'));
-    // await page.locator('#react-select-6-listbox div').nth(1).click();
+    await page.locator('.css-19bb58m').click();
+    await page.getByText('Grade I - ClassRoom 1x', { exact: true }).click();
+    await page.getByRole('button', { name: 'Save User' }).click();
+});
 
-
+test('bulk upload teacher and assign classroom', async ({ page }) => {
+    await login(page);
+    await expect(page.locator('//*[@id="dashboard"]/div[1]/div/div/div/img')).toBeVisible();
+    await page.click('a.btn.btn-estoryalight.w-100');
+    await expect(page.getByText('User Management')).toBeVisible();
+    await page.getByRole('button', { name: 'Add User' }).click();
+    await page.getByText('Bulk Add User and Assign').click();
+    await expect(page.getByRole('heading', { name: 'Add Multiple Users' })).toBeVisible();
+    await page.getByRole('link', { name: 'Download' }).click();
+    await page.getByText('browse').click();
+    const fileInput = page.locator('input[type="file"]'); // Adjust the locator to target the file input
+    await fileInput.setInputFiles('files/Bulk_teacher_upload.csv');
 });
